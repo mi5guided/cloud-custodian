e8ece0e0619e5005f18fea9d8bc1973e83fd2cca
Douglas Yoon <18539514+mi5guided@users.noreply.github.com>
2020-08-04 20:25:16 -0400

    WIP: got a simple policy to find a resource

--- dy-docker/Notes.md
+++ dy-docker/Notes.md
@@ -6,7 +6,15 @@
       -v $(pwd)/output:/home/custodian/output \
       -v $(pwd)/policy.yml:/home/custodian/policy.yml \
       --env-file <(env | grep "^AWS\|^AZURE\|^GOOGLE") \
-        cloudcustodian/c7n run -v -s /home/custodian/output /home/custodian/policy.yml
+      cloudcustodian/c7n run -v -s /home/custodian/output /home/custodian/policy.yml
+
+where 
+>-i is Keep STDIN open even if not attached (--interactive)<br>
+>-t is Allocate a pseudo-TTY (--tty)<br>
+>-v is Bind mount a volume (--volume)<br>
+>--env-file Read in a file of environment variables<br>
+>c7n -v is verbose (--verbose)<br>
+>c7n -s is output dir (--output-dir)<br>
 
 ###
 https://cloudcustodian.io/docs/
--- dy-docker/run.sh
+++ dy-docker/run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# get credentials from ~/.aws/config file
+FILELINE=`cat $HOME/.aws/config | grep aws_access_key_id`
+ACCESS_KEY=`cut -d ' ' -f3 <<< $FILELINE`
+
+FILELINE=`cat $HOME/.aws/config | grep aws_secret_access_key`
+SECRET_ACCESS_KEY=`cut -d ' ' -f3 <<< $FILELINE`
+
+
 docker run -it \
   -v $(pwd)/output:/home/custodian/output \
   -v $(pwd)/policy.yml:/home/custodian/policy.yml \
   --env-file <(env | grep "^AWS\|^AZURE\|^GOOGLE") \
-     cloudcustodian/c7n run -v -s /home/custodian/output /home/custodian/policy.yml
+  -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY \
+  -e AWS_DEFAULT_REGION=us-east-1 \
+  cloudcustodian/c7n run -v -s /home/custodian/output /home/custodian/policy.yml
