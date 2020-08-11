18e69f66a5b6d9f0961fd4f5b660b9501263df20
Douglas Yoon <18539514+mi5guided@users.noreply.github.com>
2020-08-10 21:34:41 -0400

    WIP: s3 rule set for encryption

--- dy-docker/run.sh
+++ dy-docker/run.sh
@@ -7,13 +7,19 @@ ACCESS_KEY=`cut -d ' ' -f3 <<< $FILELINE`
 FILELINE=`grep -m 1 aws_secret_access_key $HOME/.aws/config`
 SECRET_ACCESS_KEY=`cut -d ' ' -f3 <<< $FILELINE`
 
+# set policy to run
+THE_POLICY="/policies/ec2/enforce-ec2-tags.yaml"
+THE_REGIONS='all'
+THE_POLICY="/policies/s3/audit-s3-encrypted.yaml"
+THE_REGIONS='us-east-1'
+
 docker run --rm \
   -v $(pwd)/output:/home/custodian/output \
-  -v $(pwd)/policies/ec2/enforce-ec2-tags.yaml:/home/custodian/policy.yaml \
+  -v $(pwd)$THE_POLICY:/home/custodian/policy.yaml \
   --env-file <(env | grep "^AWS\|^AZURE\|^GOOGLE") \
   -e AWS_ACCESS_KEY_ID=$ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY \
   -e AWS_DEFAULT_REGION=us-east-1 \
-  cloudcustodian/c7n run --region all -v -s /home/custodian/output /home/custodian/policy.yaml
+  cloudcustodian/c7n run --region $THE_REGIONS -v -s /home/custodian/output /home/custodian/policy.yaml
 
 # if scanning only 1 region, you'll need to use keys
   # -e AWS_DEFAULT_REGION=us-east-1 \
